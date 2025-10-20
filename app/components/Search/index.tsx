cb6d78c917b590e60fa772d0b9940cb3cd915481
pxjh <pxjh03039@gmail.com>
2025-10-20 14:13:08 +0900

    검색 컴포넌트 1

--- app/components/Search/index.tsx
+++ app/components/Search/index.tsx
@@ -1,3 +1,68 @@
+import "./Search.css";
+
 export default function Search() {
-  return <div>Search Component</div>;
+  return (
+    <div className="search-container">
+      <input placeholder="검색" className="search-input" />
+
+      <ul className="search-results">
+        <li className="search-item">
+          <div className="title">약국1</div>
+          <div className="address">개봉로19길 11</div>
+        </li>
+        <li className="search-item">
+          <div className="title">약국2</div>
+          <div className="address">개봉로19길 11</div>
+        </li>
+        <li className="search-item">
+          <div className="title">약국3</div>
+          <div className="address">개봉로19길 11</div>
+        </li>
+        <li className="search-item">
+          <div className="title">약국4</div>
+          <div className="address">개봉로19길 11</div>
+        </li>
+        <li className="search-item">
+          <div className="title">약국5</div>
+          <div className="address">개봉로19길 11</div>
+        </li>
+        <li className="search-item">
+          <div className="title">약국6</div>
+          <div className="address">개봉로19길 11</div>
+        </li>
+        <li className="search-item">
+          <div className="title">약국7</div>
+          <div className="address">개봉로19길 11</div>
+        </li>
+        <li className="search-item">
+          <div className="title">약국8</div>
+          <div className="address">개봉로19길 11</div>
+        </li>
+        <li className="search-item">
+          <div className="title">약국9</div>
+          <div className="address">개봉로19길 11</div>
+        </li>
+        <li className="search-item">
+          <div className="title">약국10</div>
+          <div className="address">개봉로19길 11</div>
+        </li>
+        <li className="search-item">
+          <div className="title">약국10</div>
+          <div className="address">개봉로19길 11</div>
+        </li>
+        <li className="search-item">
+          <div className="title">약국10</div>
+          <div className="address">개봉로19길 11</div>
+        </li>
+        <li className="search-item">
+          <div className="title">약국10</div>
+          <div className="address">개봉로19길 11</div>
+        </li>
+        <li className="search-item">
+          <div className="title">약국10</div>
+          <div className="address">개봉로19길 11</div>
+        </li>
+      </ul>
+    </div>
+  );
 }
